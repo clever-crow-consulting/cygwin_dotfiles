b7245fb9b45c16cf7a8a78e424fcb22a7d5ff5e4
Dan Dye <ddye@soltra.com>
2015-10-16 16:14:49 -0400

    Remove ssh-agent badness

--- env_setup.sh
+++ env_setup.sh
@@ -13,15 +13,9 @@ ln -s ~/cygwin_dotfiles/.vimrc ~/.vimrc
 ln -s ~/cygwin_dotfiles/.vim ~/.vim
 ln -s ~/cygwin_dotfiles/.gitconfig ~/.gitconfig
 
-chmod 600 ~/.ssh/github_rsa
-chmod 600 ~/.ssh/ddye_rackspace
-
-eval `ssh-agent -s`
-ssh-add ~/.ssh/github_rsa
-ssh-add ~/.ssh/ddye_rackspace
 
 git config --global user.name 'Dan Dye'
 git config --global user.email ddye@soltra.com
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/clever-crow-consulting/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
